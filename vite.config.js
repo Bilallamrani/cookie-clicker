bb8be11f1a4ecdb42959a84ea606258bab068582
Bilal Lamrani <bilal-lamrani@hotmail.com>
2023-01-18 13:52:38 +0100

    commit vite js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  root: "src",
-  base: "/deploying-vite-project-example/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        css: resolve(__dirname, "src/index.css"),
-      },
-    },
-  
-    outDir: resolve(__dirname, "dist"),
-    emptyOutDir: true,
-  },
-});
+  base: '/cookie-clicker/',
+})
