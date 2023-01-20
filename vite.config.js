6c7c8bb57f2640d5245942c6100b998cfe60814e
Bilal Lamrani <bilal-lamrani@hotmail.com>
2023-01-20 10:45:46 +0100

    tailwind

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
-  base: "",
+  base: "/cookie-clicker/",
   build: {
     rollupOptions: {
       input: {
