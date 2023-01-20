284b1cd67e2c195503a1d68e75855a5e1b8f992e
Bilal Lamrani <bilal-lamrani@hotmail.com>
2023-01-20 10:27:55 +0100

    committailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
-  base: '/cookie-clicker/',
-})
+  root: "src",
+  base: "",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
+});
